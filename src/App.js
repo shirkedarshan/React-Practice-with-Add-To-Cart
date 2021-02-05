8c455c077f7a86baa9e47b0fe19313ad91632af4
Darshan Shirke <shirkedarshan@gmail.com>
2021-02-05 17:55:19 +0530

    Hooks : useEffect() Part 2

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ function App() {
     const [count, setCount] = useState(0)
     const [color, setColor] = useState("")
     
-    function increment() {
-        setCount(prevCount => prevCount + 1)
-    }
-    
-    function decrement() {
-        setCount(prevCount => prevCount - 1)
-    }
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCount(prevCount => prevCount + 1)
+        }, 1000)
+        return () => clearInterval(intervalId)
+    }, [])
     
     useEffect(() => {
         setColor(randomcolor())
@@ -20,8 +19,6 @@ function App() {
     return (
         <div>
             <h1 style={{color: color}}>{count}</h1>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
         </div>
     )
 }
