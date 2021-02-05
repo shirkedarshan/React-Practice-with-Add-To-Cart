9bf45e1c8e37b1928b3c56bc19110ff6952dda1c
Darshan Shirke <shirkedarshan@gmail.com>
2021-02-05 21:24:15 +0530

    Speed Typing Game : Part 4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import "./App.css"
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 /**
  * Challenge:
  * 
- * Create a function to calculate the number of separate words in the `text` state
- * For now, just console.log the word count when the button gets clicked to test it out.
+ * 1. Create state to hold the current value of the countdown timer.
+ *    Display this time in the "Time Remaining" header
+ * 
+ * 2. Set up an effect that runs every time the `timeRemaining` changes
+ *    The effect should wait 1 second, then decrement the `timeRemaining` by 1
+ * 
+ *    Hint: use `setTimeout` instead of `setInterval`. This will help you avoid
+ *    a lot of extra work.
+ * 
+ *    Warning: there will be a bug in this, but we'll tackle that next
+ * 
+ * 3. Make it so the effect won't run if the time is already at 0
  */
 
 function App() {
     const [text, setText] = useState("")
+    const [timeRemaining, setTimeRemaining] = useState(5)
     
     function handleChange(e) {
         const {value} = e.target
@@ -18,10 +29,16 @@ function App() {
     
     function calculateWordCount(text) {
         const wordsArr = text.trim().split(" ")
-        console.log(wordsArr.length)
-        // return wordsArr.length
         return wordsArr.filter(word => word !== "").length
     }
+    
+    useEffect(() => {
+        if(timeRemaining > 0) {
+            setTimeout(() => {
+                setTimeRemaining(time => time - 1)
+            }, 1000)
+        }
+    }, [timeRemaining])
     
     return (
         <div>
@@ -30,8 +47,8 @@ function App() {
                 onChange={handleChange}
                 value={text}
             />
-            <h4>Time remaining: ???</h4>
-            <button onClick={() => (calculateWordCount(text))}>Start</button>
+            <h4>Time remaining: {timeRemaining}</h4>
+            <button onClick={() => console.log(calculateWordCount(text))}>Start</button>
             <h1>Word count: ???</h1>
         </div>
     )
