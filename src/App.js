3bd8ca0055e85df8e8cf818ab7c37b5367cb633e
Darshan Shirke <shirkedarshan@gmail.com>
2021-02-05 17:32:50 +0530

    Hooks : useState() Part 3

--- src/App.js
+++ src/App.js
@@ -1,46 +1,44 @@
 import React, {useState} from "react"
 
-// Convert the class below to a functional component that uses the useState hook to initalize a count vartiable to 0 and display the count on the screen.
-// Don't worry about the part where the button changes the count quite yet, that's what you're here to learn about!
-
 function App() {
-    const [count, setCount] = useState(0)
-    const [answer, setAnswer] = useState("Yes")
+    const [inputData, setInputData] = useState({firstName: "", lastName: ""})
+    const [contactsData, setContactsData] = useState([])
     
-    function increment() {
-        setCount(prevCount => prevCount + 1)
+    function handleChange(event) {
+        const {name, value} = event.target
+        setInputData(prevInputData => ({...prevInputData, [name]: value}))
     }
     
-    function decrement() {
-        setCount(prevCount => prevCount - 1)
+    function handleSubmit(event) {
+        event.preventDefault()
+        setContactsData(prevContacts => [...prevContacts, inputData])
+        console.log( inputData )
+    
     }
+    
+    const contacts = contactsData.map(contact => <h2 key={contact.firstName + contact.lastName}>{contact.firstName} {contact.lastName}</h2>)
     
     return (
-        <div>
-            <h1>{count}</h1>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
-        </div>
+        <>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    placeholder="First Name"
+                    name="firstName" 
+                    value={inputData.firstName}
+                    onChange={handleChange}
+                />
+                <input 
+                    placeholder="Last Name"
+                    name="lastName" 
+                    value={inputData.lastName}
+                    onChange={handleChange}
+                />
+                <br />
+                <button>Add contact</button>
+            </form>
+            {contacts}
+        </>
     )
 }
-
-// class App extends React.Component {
-//     constructor() {
-//         super()
-//         this.state = {
-//             count: 0,
-//             answer: "Yes"
-//         }
-//     }
-    
-//     render() {
-//         return (
-//             <div>
-//                 <h1>{this.state.count}</h1>
-//                 <button>Change!</button>
-//             </div>
-//         )
-//     }
-// }
 
 export default App
