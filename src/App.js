e66a39c9e46daeb07c5414dfdf4f9ed4c40727af
Darshan Shirke <shirkedarshan@gmail.com>
2021-02-05 21:51:54 +0530

    Speed Typing Game : Part 5

--- src/App.js
+++ src/App.js
@@ -4,23 +4,14 @@ import React, {useState, useEffect} from "react"
 /**
  * Challenge:
  * 
- * 1. Create state to hold the current value of the countdown timer.
- *    Display this time in the "Time Remaining" header
- * 
- * 2. Set up an effect that runs every time the `timeRemaining` changes
- *    The effect should wait 1 second, then decrement the `timeRemaining` by 1
- * 
- *    Hint: use `setTimeout` instead of `setInterval`. This will help you avoid
- *    a lot of extra work.
- * 
- *    Warning: there will be a bug in this, but we'll tackle that next
- * 
- * 3. Make it so the effect won't run if the time is already at 0
+ * Make it so clicking the Start button starts the timer instead of it starting on refresh
+ * (Hint: use a new state variable to indicate if the game should be running or not)
  */
 
 function App() {
     const [text, setText] = useState("")
     const [timeRemaining, setTimeRemaining] = useState(5)
+    const [isTimeRunning, setIsTimeRunning] = useState(false)
     
     function handleChange(e) {
         const {value} = e.target
@@ -33,12 +24,14 @@ function App() {
     }
     
     useEffect(() => {
-        if(timeRemaining > 0) {
+        if(isTimeRunning && timeRemaining > 0) {
             setTimeout(() => {
                 setTimeRemaining(time => time - 1)
             }, 1000)
+        } else if(timeRemaining === 0) {
+            setIsTimeRunning(false)
         }
-    }, [timeRemaining])
+    }, [timeRemaining, isTimeRunning])
     
     return (
         <div>
@@ -48,7 +41,7 @@ function App() {
                 value={text}
             />
             <h4>Time remaining: {timeRemaining}</h4>
-            <button onClick={() => console.log(calculateWordCount(text))}>Start</button>
+            <button onClick={() => setIsTimeRunning(true)}>Start</button>
             <h1>Word count: ???</h1>
         </div>
     )
