a924c9c04782e9bd13e5d8cc899d198da4fe5466
Darshan Shirke <shirkedarshan@gmail.com>
2021-02-05 20:37:06 +0530

    Speed Typing Game : Part 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from "react"
 import "./App.css"
+import React, {useState} from "react"
+
+/**
+ * Challenge:
+ * 
+ * Create a function to calculate the number of separate words in the `text` state
+ * For now, just console.log the word count when the button gets clicked to test it out.
+ */
 
 function App() {
+    const [text, setText] = useState("")
+    
+    function handleChange(e) {
+        const {value} = e.target
+        setText(value)
+    }
+    
+    function calculateWordCount(text) {
+        const wordsArr = text.trim().split(" ")
+        console.log(wordsArr.length)
+        // return wordsArr.length
+        return wordsArr.filter(word => word !== "").length
+    }
+    
     return (
         <div>
             <h1>How fast do you type?</h1>
-            <textarea />
-            <h4>Time reminaing: ???</h4>
-            <button>Start</button>
+            <textarea
+                onChange={handleChange}
+                value={text}
+            />
+            <h4>Time remaining: ???</h4>
+            <button onClick={() => (calculateWordCount(text))}>Start</button>
             <h1>Word count: ???</h1>
         </div>
     )
