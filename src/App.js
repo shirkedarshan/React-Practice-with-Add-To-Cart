ecd868c0fcf775394dafe3712a9246ab0ded1c00
Darshan Shirke <shirkedarshan@gmail.com>
2021-02-06 17:11:06 +0530

    useRef()

--- src/App.js
+++ src/App.js
@@ -1,67 +1,30 @@
-import "./App.css"
-import React, {useState, useEffect} from "react"
-
-/**
- * Challenge:
- * 
- * Make it so clicking the Start button starts the timer instead of it starting on refresh
- * (Hint: use a new state variable to indicate if the game should be running or not)
- */
+import React, {useState, useRef} from "react"
 
 function App() {
-
-    const STARTING_TIME = 5
-
-    const [text, setText] = useState("")
-    const [timeRemaining, setTimeRemaining] = useState( STARTING_TIME)
-    const [isTimeRunning, setIsTimeRunning] = useState(false)
-    const [count, setCount] = useState("0")
-
-    function handleChange(e) {
-        const {value} = e.target
-        setText(value)
-    }
-
-    function startClock() {
-        setIsTimeRunning(true)
-        setTimeRemaining( STARTING_TIME)
-        setText("")
-        document.getElementById('anc').disabled = false
-        document.getElementById('start').disabled = true
+    const [newTodoValue, setNewTodoValue] = useState("")
+    const [todosList, setTodosList] = useState([])
+    const inputRef = useRef(null)
+    
+    function handleChange(event) {
+        setNewTodoValue(event.target.value)
     }
     
-    function calculateWordCount(text) {
-        const wordsArr = text.trim().split(" ")
-        return wordsArr.filter(word => word !== "").length
+    function addTodo(event) {
+        event.preventDefault()
+        setTodosList(prevTodosList => [...prevTodosList, newTodoValue])
+        setNewTodoValue("")
+        inputRef.current.focus()
     }
     
-    useEffect(() => {
-        if(isTimeRunning && timeRemaining > 0) {    
-            setCount(0)
-            setTimeout(() => {
-                setTimeRemaining(time => time - 1)
-            }, 1000)
-        } else if(timeRemaining === 0) {
-            setIsTimeRunning(false)
-            setCount(calculateWordCount(text))
-            document.getElementById('anc').disabled = true 
-            document.getElementById('start').disabled = false
-        }
-
-    }, [timeRemaining, isTimeRunning])
+    const allTodos = todosList.map(todo => <p key={todo}>{todo}</p>)
     
     return (
         <div>
-            <h1>How fast do you type?</h1>
-            <textarea
-                onChange={handleChange}
-                value={text}
-                disabled={true}
-                id={"anc"}
-            />
-            <h4>Time remaining: {timeRemaining}</h4>
-            <button id={"start"} onClick={startClock}>Start</button>
-            <h1>Word count: {count}</h1>
+            <form>
+                <input ref={inputRef} type="text" name="todo" value={newTodoValue} onChange={handleChange}/>
+                <button onClick={addTodo}>Add todo item</button>
+            </form>
+            {allTodos}
         </div>
     )
 }
