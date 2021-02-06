76767f0102a71376dfa1935edc759ebf596e6373
Darshan Shirke <shirkedarshan@gmail.com>
2021-02-06 18:26:21 +0530

    useContext()

--- src/App.js
+++ src/App.js
@@ -1,73 +1,16 @@
-import React, {useState, useEffect, useRef} from "react"
+import React from "react"
+import "./App.css"
 
-/**
- * Challenge:
- * 
- * Make the input box focus (DOM elements have a method called .focus()) 
- * immediately when the game starts
- */
+import Header from "./Header"
+import Button from "./Button"
 
 function App() {
-    const STARTING_TIME = 5
-    
-    const [text, setText] = useState("")
-    const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
-    const [isTimeRunning, setIsTimeRunning] = useState(false)
-    const [wordCount, setWordCount] = useState(0)
-    const textBoxRef = useRef(null)
-    
-    function handleChange(e) {
-        const {value} = e.target
-        setText(value)
-    }
-    
-    function calculateWordCount(text) {
-        const wordsArr = text.trim().split(" ")
-        return wordsArr.filter(word => word !== "").length
-    }
-    
-    function startGame() {
-        setIsTimeRunning(true)
-        setTimeRemaining(STARTING_TIME)
-        setText("")
-        textBoxRef.current.disabled = false
-        textBoxRef.current.focus()
-    }
-    
-    function endGame() {
-        setIsTimeRunning(false)
-        setWordCount(calculateWordCount(text))
-    }
-    
-    useEffect(() => {
-        if(isTimeRunning && timeRemaining > 0) {
-            setTimeout(() => {
-                setTimeRemaining(time => time - 1)
-            }, 1000)
-        } else if(timeRemaining === 0) {
-            endGame()
-        }
-    }, [timeRemaining, isTimeRunning])
-    
     return (
         <div>
-            <h1>How fast do you type?</h1>
-            <textarea
-                ref={textBoxRef}
-                onChange={handleChange}
-                value={text}
-                disabled={!isTimeRunning}
-            />
-            <h4>Time remaining: {timeRemaining}</h4>
-            <button 
-                onClick={startGame}
-                disabled={isTimeRunning}
-            >
-                Start
-            </button>
-            <h1>Word count: {wordCount}</h1>
+            <Header />
+            <Button />
         </div>
     )
 }
 
-export default App
+export default App  
