9505d98cba69253fc015a2d18fd0cc3c158862f7
Darshan Shirke <shirkedarshan@gmail.com>
2021-02-09 22:06:15 +0530

    Welcome to React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from "react"
+import React, {useState, useEffect, useRef} from "react"
 
-function App (){
-        return (
-            <div>
-                <h2>Welcome to Hooks</h2>
-                <hr/>
-                <p>useState</p>
-                <p>useEffect</p>
-                <p>useContext</p>
-                <p>useRef</p>
-                <p>useReducer</p>
-                <hr/>
-                <p>useMemo</p>
-                <p>useCallback</p>
-                <p>useImperativeHandle</p>
-                <p>useLayoutEffect</p>
-                <p>useDebugValue</p>
-                <hr/>
-            </div>
-        )    
+function App() {
+    
+    return (
+        <div>
+            <h1>Welcome to React Router</h1>
+        </div>
+    )
 }
 
 export default App
