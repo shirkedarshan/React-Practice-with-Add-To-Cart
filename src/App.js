1a0b92605f5e018193f2c3673c37fb62315afb41
Darshan Shirke <shirkedarshan@gmail.com>
2021-02-09 21:52:32 +0530

    Speed Typing Game : Part 8

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react"
-import Menu from "./Menu"
-import Favorite from "./Favorite"
+import React, {useState, useEffect, useRef} from "react"
+import useWordGame from "./useWordGame"
 
 function App() {
+    const {
+        textBoxRef, 
+        handleChange, 
+        text, 
+        isTimeRunning, 
+        timeRemaining, 
+        startGame, 
+        wordCount
+    } = useWordGame(5)
+    
     return (
         <div>
-            <Menu />
-            <hr />
-            <Favorite />
+            <h1>How fast do you type?</h1>
+            <textarea
+                ref={textBoxRef}
+                onChange={handleChange}
+                value={text}
+                disabled={!isTimeRunning}
+            />
+            <h4>Time remaining: {timeRemaining}</h4>
+            <button 
+                onClick={startGame}
+                disabled={isTimeRunning}
+            >
+                Start
+            </button>
+            <h1>Word count: {wordCount}</h1>
         </div>
     )
 }
