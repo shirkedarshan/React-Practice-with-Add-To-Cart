e1da51f89f66ccbb45f5c577174244c1fb05c449
Darshan Shirke <shirkedarshan@gmail.com>
2021-02-10 15:17:18 +0530

    Router : Link Switch Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React, {useState, useEffect, useRef} from "react"
+import React from "react"
+import {Link, Switch, Route} from "react-router-dom"
 
-function App() {
-    
+import Home from "./Home"
+import About from "./About"
+
+function App() {    
     return (
         <div>
-            <h1>Welcome to React Router</h1>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            
+            <Switch>
+                {/* <Route exact path="/" render={() => <Home />} />
+                <Route path="/about" render={() => <About />} />
+
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} /> */}
+
+                <Route exact path="/"><Home /></Route>
+                <Route path="/about"><About /></Route>
+
+            </Switch>
         </div>
     )
 }
