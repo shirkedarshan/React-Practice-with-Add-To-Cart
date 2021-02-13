011a84e05bb98ffd0287f74b7e48f0dec184e470
Darshan Shirke <shirkedarshan@gmail.com>
2021-02-13 16:53:25 +0530

    useHistory()

--- src/pages/services/ServiceDetail.js
+++ src/pages/services/ServiceDetail.js
@@ -1,17 +1,28 @@
 import React from "react"
-import {useParams} from "react-router-dom"
+import {useParams, useHistory} from "react-router-dom"
+
 import servicesData from "./servicesData"
 
-function ServiceDetail(props) {
+function ServiceDetail() {
     const {serviceId} = useParams()
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+    const history = useHistory()
     const thisService = servicesData.find(service => service._id === serviceId)
+
+    
+    function handleClick() {
+        console.log("Submitting...")
+        setTimeout(() => {
+            history.push("/services")
+        }, 2000)
+        history.go(-1)
+    }
     
     return (
         <div>
             <h1>Service Detail Page</h1>
             <h3>{thisService.name} - ${thisService.price}</h3>
             <p>{thisService.description}</p>
+            <button onClick={handleClick}>Go back to all services</button>
         </div>
     )
 }
