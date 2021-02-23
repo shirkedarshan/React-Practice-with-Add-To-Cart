abeb095a83cfc3b9d85c8b599b429acc96b1204c
Darshan Shirke <shirkedarshan@gmail.com>
2021-02-23 17:22:47 +0530

    Route Practice 1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
-import React, {useState} from "react"
-import {Link, Switch, Route, Redirect} from "react-router-dom"
+import React from "react"
+import {Link, Switch, Route} from "react-router-dom"
+import Home from "./Home"
+import Products from "./Products"
 
-// https://reacttraining.com/react-router/web/example/auth-workflow
-
-function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
+function App() {    
     return (
         <div>
-            <Link to="/">Home</Link>
-            <Link to="/private">Private</Link>
-            
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/products">Products</Link>
+            </nav>
             <Switch>
                 <Route exact path="/">
-                    <h1>Home page, anyone is allowed here</h1>
+                    <Home />
                 </Route>
-                <Route path="/private">
-                    {
-                        isLoggedIn ?
-                        <h1>Protected page, must be logged in to be here</h1> :
-                        <Redirect to="/login" />
-                    }
-                </Route>
-                <Route path="/login">
-                    <button onClick={() => setIsLoggedIn(true)}>Log in</button>
+                <Route path="/products">
+                    <Products />
                 </Route>
             </Switch>
+            
+            
+            {/*
+                Route between 2 components depending on the path:
+                1. Home page ("/")
+                2. Products Page ("/products")
+                
+                ** Home Page **
+                • Can be a basic home page, maybe welcoming the user to your site and giving some background about yourself as a developer
+                
+                ** Products Page **
+                • Using an array of data for your products (provided for you), map over that data to show each item and some information on the products page as a list of available products.
+            */}
+            
         </div>
     )
 }
