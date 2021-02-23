b7aca7e60b520be6382caebc5ea3e1c0dfcb502b
Darshan Shirke <shirkedarshan@gmail.com>
2021-02-23 18:40:14 +0530

    WELCOME TO CAPSTONE PROJECT

--- src/App.js
+++ src/App.js
@@ -4,9 +4,25 @@ function App() {
     
     return (
         <div>
-            <h1>Welcome to React Router</h1>
+            <h1>Welcome to React Capstone Project</h1>
         </div>
     )
 }
 
 export default App
+
+// # Image attributions:
+// https://picsum.photos/
+
+// https://unsplash.com/
+
+// # GitHub repo of images and JSON file
+// https://github.com/bobziroll/scrimba-react-bootcamp-images
+
+// # Icon library
+// https://remixicon.com/
+// https://cdn.jsdelivr.net/npm/remixicon@2.3.0/fonts/remixicon.css
+
+// # Libraries
+// * React Router - https://reacttraining.com/react-router/web/guides/quick-start
+// * PropTypes - https://reactjs.org/docs/typechecking-with-proptypes.html
