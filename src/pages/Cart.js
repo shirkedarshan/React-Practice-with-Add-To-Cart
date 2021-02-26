55f39255a23144b5c17262994ec0186eba4cf7e2
Darshan Shirke <shirkedarshan@gmail.com>
2021-02-26 22:21:07 +0530

    Cart Page Calculate Total Cost

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,12 +7,14 @@ function Cart() {
     const cartItemElements = cartItems.map( item => (
         <CartItem key="item.id" item={item} />
         ))
+    const totalCost = 5.99 * cartItems.length
+    const totalCostDisplay = totalCost.toLocaleString("en-US", {style: "currency", currency: "USD"})
     
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {cartItemElements}
-            <p className="total-cost">Total: </p>
+            <p className="total-cost">Total: {totalCostDisplay}</p>
             <div className="order-button">
                 <button>Place Order</button>
             </div>
