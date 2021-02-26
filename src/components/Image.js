0b5f51e5a68206615d8c82c030159a6ff63c3bd7
Darshan Shirke <shirkedarshan@gmail.com>
2021-02-26 17:07:57 +0530

    Image : Display icon hover

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,13 +2,17 @@ import React , {useState} from "react"
 
 function Image( {className , img} ){
     const [hovered,setHovered] = useState(false)
+    const heartIcon = hovered && <i className="ri-heart-line favorite"></i> 
+    const plusIcon =  hovered && <i className="ri-heart-line plus"></i> 
 
     console.log(hovered)
 
     return(
         <div className={`${className} image-container `} 
-        onMouseEnter={()=> setHovered(true)}
-        ouMouseLeave={()=> setHovered(false)}>  
+            onMouseEnter={()=> setHovered(true)}
+            ouMouseLeave={()=> setHovered(false)}>  
+            {heartIcon}
+            {plusIcon}
             <img src={img.url} className="image-grid"/>
         </div>
     )
